6f9da2b37f89f9e26c49206aed2376565f036779
Gergana Dimitrova <geri0547@abv.bg>
2023-11-10 20:14:31 +0200

    Initial commit

---
+++ services-for-you/src/main/java/com/example/servicesforyou/ServicesForYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicesforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesForYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesForYouApplication.class, args);
+    }
+
+}
---
+++ services-for-you/src/test/java/com/example/servicesforyou/ServicesForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicesforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
