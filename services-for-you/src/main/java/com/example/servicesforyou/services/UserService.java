7fdda4a30a360a6e71a972ccc1944ef1f1cc9097
Gergana Dimitrova <geri0547@abv.bg>
2023-11-21 18:54:59 +0200

    Added Service for Users

---
+++ services-for-you/src/main/java/com/example/servicesforyou/services/UserService.java
@@ -0,0 +1,4 @@
+package com.example.servicesforyou.services;
+
+public interface UserService {
+}
---
+++ services-for-you/src/main/java/com/example/servicesforyou/services/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.servicesforyou.services.impl;
+
+import com.example.servicesforyou.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
